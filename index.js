b7789ea15d3b6b5ca8e573fb191991f0c8cf7059
sweetcherry34 <128580272+sweetcherry34@users.noreply.github.com>
2023-04-25 09:23:06 +0200

    #

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$('#menu').click(function(){
-$(this).toggleClass('fa-times');
-$('header').toggleClass('toggle');
-});
+        $('#menu').click(function () {
+                $(this).toggleClass('fa-times');
+                $('header').toggleClass('toggle');
+        });
 
-$(window).on('scroll load',function(){
+        $(window).on('scroll load', function () {
 
-$('#menu').removeClass('fa-times');
-$('header').removeClass('toggle');
+                $('#menu').removeClass('fa-times');
+                $('header').removeClass('toggle');
 
-});
+        });
 
 });
 
 //smooth scrolling
-$('a[href*="#"]').on('click',function(e){
-   
+$('a[href*="#"]').on('click', function (e) {
+
         e.preventDefault();
         $('html, body').animate({
 
-scrollTop: $($(this).attr(' href')).offset( ).top, 
+                scrollTop: $($(this).attr(' href')).offset().top,
 
         },
-        500,
-         'linear'
+                500,
+                'linear'
         );
-         
+
 });
 
-function sendEmail(){
-        Email.send({
-                    Host : "smtp.gmail.com",
-                    Username : "chidochemoyorushwaya@gmail.com",
-                    Password : "king3ston3",
-                    To : 'chidochemoyorushwaya@gmail.com',
-                    From : document.getElementById("email").value ,
-                    Subject : "New Contact Form Enquiry",
-                    Body : "Name" + document.getElementById("name").value 
-                            + "<br> Email: " + document.getElementById("email").value 
-                            + "<br> Project: " + document.getElementById("project").value 
-                            + "<br> Message: " + document.getElementById("message ").value 
-                }).then( 
-                message => alert(" Message Sent Succesfully") 
-                );
-    }
+var typed = new Typed(".input", {
+        strings:[ "I'm Chidochemoyo Rushwaya.","I am a Digital Makerter.", "and also a UX/UI Designer."],
+        typeSpeed: 70,
+        backSpeed: 60,
+        loop: true
+    });
+
